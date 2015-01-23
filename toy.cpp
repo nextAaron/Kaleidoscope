d5a6ca1c093e46a06b2914d3248750e8887c829b
Aaron Chen <nextAaron@gmail.com>
2015-01-23 03:07:19 -0800

    Chapter 5

--- toy.cpp
+++ toy.cpp
@@ -27,7 +27,13 @@ enum Token {
 	tok_extern = -3,
 	//primary
 	tok_identifier = -4,
-	tok_number = -5
+	tok_number = -5,
+	//control
+	tok_if = -6,
+	tok_then = -7,
+	tok_else = -8,
+	tok_for = -9,
+	tok_in = -10
 };
 
 static std::string IdentifierStr;
@@ -40,25 +46,36 @@ static int get_tok() {//Return the next token from standard input
 		LastChar = getchar();
 
 	if (isalpha(LastChar)) {//[a-zA-Z][a-zA-Z0-9]*
-		IdentifierStr = LastChar;
-		while (isalnum(LastChar = getchar()))
-			IdentifierStr += LastChar;
+		IdentifierStr.clear();
+		do {
+			IdentifierStr += static_cast<char>(LastChar);
+		} while (isalnum(LastChar = getchar()));			
 
 		if (IdentifierStr == "def")
 			return tok_def;
 		if (IdentifierStr =="extern")
-				return tok_extern;
+			return tok_extern;
+		if (IdentifierStr == "if")
+			return tok_if;
+		if (IdentifierStr == "then")
+			return tok_then;
+		if (IdentifierStr == "else")
+			return tok_else;
+		if (IdentifierStr == "for")
+			return tok_for;
+		if (IdentifierStr == "in")
+			return tok_in; 
 		return tok_identifier;
 	}
 
 	if (isdigit(LastChar) || LastChar == '.') {//[0-9.]+
 		std::string NumStr;
 		do {
-			NumStr += LastChar;
+			NumStr += static_cast<char>(LastChar);
 			LastChar = getchar();
 		} while (isdigit(LastChar) || LastChar == '.');
 
-		NumVal = stod(NumStr, 0);
+		NumVal = stod(NumStr);
 		return tok_number;
 	}
 
@@ -100,9 +117,9 @@ namespace {
 	};
 
 	class BinaryExprAST : public ExprAST {//Binary operators
-		char Op;
 		ExprAST* LHS;
 		ExprAST* RHS;
+		char Op;
 	public:
 		BinaryExprAST(char op, ExprAST* lhs, ExprAST* rhs) : Op(op), LHS(lhs), RHS(rhs) {}
 		llvm::Value* Codegen();
@@ -114,6 +131,27 @@ namespace {
 	public:
 		CallExprAST(const std::string& callee, std::vector<ExprAST*>& args) : Callee(callee), Args(args) {}
 		llvm::Value* Codegen();
+	};
+
+	class IfExprAST : public ExprAST {
+		ExprAST* Cond;
+		ExprAST* Then;
+		ExprAST* Else;
+	public:
+		IfExprAST(ExprAST* cond, ExprAST* then, ExprAST* _else) : Cond(cond), Then(then), Else(_else) {}
+		virtual llvm::Value* Codegen();
+	};
+
+	class ForExprAST : public ExprAST {
+		std::string VarName;
+		ExprAST* Start;
+		ExprAST* End;
+		ExprAST* Step;
+		ExprAST* Body;
+	public:
+		ForExprAST(const std::string& varname, ExprAST* start, ExprAST* end, ExprAST* step, ExprAST* body) :
+			VarName(varname), Start(start), End(end), Step(step), Body(body) {}
+		virtual llvm::Value* Codegen();
 	};
 
 	class PrototypeAST {//Function interface
@@ -143,7 +181,7 @@ static std::map<char, int> BinopPrecedence;//
 static int GetTokPrecedence() {//Get the precedence of the pending binary operator token
 	if (!isascii(CurTok))
 		return -1;
-	auto TokPrec = BinopPrecedence[CurTok];
+	auto TokPrec = BinopPrecedence[static_cast<char>(CurTok)];
 	if (TokPrec <= 0)
 		return -1;
 	return TokPrec;
@@ -209,10 +247,95 @@ static ExprAST* ParseParenExpr() {
 	return V;
 }
 
+/*ifexpr ::=
+		'if' expression 'then' expression 'else' expression
+ */
+static ExprAST* ParseIfExpr() {
+	getNextToken();//Eat 'if'
+	auto Cond = ParseExpression();
+	if (!Cond)
+		return nullptr;
+	if (CurTok != tok_then) {
+		Error("expected then");
+		return nullptr;
+	}
+	getNextToken();//Eat 'then'
+
+	auto Then = ParseExpression();
+	if (!Then)
+		return nullptr;
+	if (CurTok != tok_else) {
+		Error("expected else");
+		return nullptr;
+	}
+	getNextToken();//Eat 'else'
+
+	auto Else = ParseExpression();
+	if (!Else)
+		return nullptr;
+
+	return new IfExprAST(Cond, Then, Else);
+}
+
+/*forexpr
+		::= 'for' identifier '=' expr ',' expr (',' expr)? 'in' expression
+ */
+static ExprAST* ParseForExpr() {
+	getNextToken();//Eat 'for'
+	if (CurTok != tok_identifier) {
+		Error("expected identifier after for");
+		return nullptr;
+	}
+
+	auto IdName = IdentifierStr;
+	getNextToken();//Eat identifier
+
+	if (CurTok != '=') {
+		Error("expected '=' after for");
+		return nullptr;
+	}
+	getNextToken();//Eat '='
+
+	auto Start = ParseExpression();
+	if (!Start)
+		return nullptr;
+	if (CurTok != ',') {
+		Error("expected ',' after for start");
+		return nullptr;
+	}
+	getNextToken();
+
+	auto End = ParseExpression();
+	if (!End)
+		return nullptr;
+
+	ExprAST* Step = nullptr;
+	if (CurTok == ',') {
+		getNextToken();
+		Step = ParseExpression();
+		if (!Step)
+			return nullptr;
+	}
+
+	if (CurTok != tok_in) {
+		Error("expected 'in' after for");
+		return nullptr;
+	}
+	getNextToken();//Eat 'in'
+
+	auto Body = ParseExpression();
+	if (!Body)
+		return nullptr;
+
+	return new ForExprAST(IdName, Start, End, Step, Body);
+}
+
 /*primary
 		::= identifierexpr
 		::= numberexpr
 		::= parenexpr
+		::= ifexpr
+		::= forexpr
  */
 static ExprAST* ParsePrimary() {
 	switch (CurTok) {
@@ -222,6 +345,10 @@ static ExprAST* ParsePrimary() {
 			return ParseNumberExpr();
 		case '(':
 			return ParseParenExpr();
+		case tok_if:
+			return ParseIfExpr();
+		case tok_for:
+			return ParseForExpr();
 		default:
 			Error("unknown token when expecting an expression");
 			return nullptr;
@@ -237,7 +364,7 @@ static ExprAST* ParseBinOpRHS(int ExprPrec, ExprAST* LHS) {
 		if (TokPrec < ExprPrec)
 			return LHS;
 		
-		char BinOp = CurTok;
+		auto BinOp = static_cast<char>(CurTok);
 		getNextToken();
 
 		auto RHS = ParsePrimary();
@@ -366,7 +493,7 @@ llvm::Value* BinaryExprAST::Codegen() {
 }
 
 llvm::Value* CallExprAST::Codegen() {
-	llvm::Function* CalleeF = TheModule->getFunction(Callee);
+	auto CalleeF = TheModule->getFunction(Callee);
 	if (!CalleeF) {
 		Error("unknown function referenced");
 		return nullptr;
@@ -383,6 +510,142 @@ llvm::Value* CallExprAST::Codegen() {
 			return nullptr;
 	}
 	return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+llvm::Value* IfExprAST::Codegen() {
+	auto CondV = Cond->Codegen();
+	if (!CondV)
+		return nullptr;
+	
+	//Convert condition to a bool function
+	CondV = Builder.CreateFCmpONE(CondV, llvm::ConstantFP::get(llvm::getGlobalContext(), llvm::APFloat(0.0)), "ifcond");
+	auto TheFunction = Builder.GetInsertBlock()->getParent();
+
+	//Create block for then and else
+	auto ThenBB = llvm::BasicBlock::Create(llvm::getGlobalContext(), "then", TheFunction);
+	auto ElseBB = llvm::BasicBlock::Create(llvm::getGlobalContext(), "else");
+	auto MergeBB = llvm::BasicBlock::Create(llvm::getGlobalContext(), "ifcont");
+	
+	Builder.CreateCondBr(CondV, ThenBB, ElseBB);
+	Builder.SetInsertPoint(ThenBB);
+	auto ThenV = Then->Codegen();
+	if (!ThenV)
+		return nullptr;
+	Builder.CreateBr(MergeBB);
+	ThenBB = Builder.GetInsertBlock();
+
+	TheFunction->getBasicBlockList().push_back(ElseBB);
+	Builder.SetInsertPoint(ElseBB);
+	auto ElseV = Else->Codegen();
+	if (!ElseV)
+		return nullptr;
+	Builder.CreateBr(MergeBB);
+	ElseBB = Builder.GetInsertBlock();
+
+	TheFunction->getBasicBlockList().push_back(MergeBB);
+	Builder.SetInsertPoint(MergeBB);
+
+	auto PN = Builder.CreatePHI(llvm::Type::getDoubleTy(llvm::getGlobalContext()), 2, "iftmp");
+	PN->addIncoming(ThenV, ThenBB);
+	PN->addIncoming(ElseV, ElseBB);
+	return PN;
+}
+
+llvm::Value* ForExprAST::Codegen() {
+	//Output this as:
+	//  ...
+	//  start = startexpr
+	//  goto loop
+	//loop:
+	//  variable = phi [start, loopheader], [nextvariable, loopend]
+	//  ...
+	//  bodyexpr
+	//  ...
+	//loopend:
+	//  step = stepexpr
+	//  nextvariable = variable + step
+	//  endcond = endexpr
+	//  br endcond, loop, endloop
+	//outloop:
+
+	//Emit the start code first, without 'variable' in scope.
+	auto StartVal = Start->Codegen();
+	if (!StartVal)
+		return nullptr;
+
+	//Make the new basic block for the loop header, inserting after current
+	//block.
+	auto TheFunction = Builder.GetInsertBlock()->getParent();
+	auto PreheaderBB = Builder.GetInsertBlock();
+	auto LoopBB =
+		llvm::BasicBlock::Create(llvm::getGlobalContext(), "loop", TheFunction);
+
+	//Insert an explicit fall through from the current block to the LoopBB.
+	Builder.CreateBr(LoopBB);
+
+	//Start insertion in LoopBB.
+	Builder.SetInsertPoint(LoopBB);
+
+	//Start the PHI node with an entry for Start.
+	auto Variable = Builder.CreatePHI(llvm::Type::getDoubleTy(llvm::getGlobalContext()),
+																				2, VarName.c_str());
+	Variable->addIncoming(StartVal, PreheaderBB);
+
+	//Within the loop, the variable is defined equal to the PHI node.  If it
+	//shadows an existing variable, we have to restore it, so save it now.
+	auto OldVal = NamedValues[VarName];
+	NamedValues[VarName] = Variable;
+
+	//Emit the body of the loop.  This, like any other expr, can change the
+	//current BB.  Note that we ignore the value computed by the body, but don't
+	//allow an error.
+	if (Body->Codegen() == nullptr)
+		return nullptr;
+
+	//Emit the step value.
+	llvm::Value *StepVal;
+	if (Step) {
+		StepVal = Step->Codegen();
+		if (!StepVal)
+			return nullptr;
+	} else {
+		//If not specified, use 1.0.
+		StepVal = llvm::ConstantFP::get(llvm::getGlobalContext(), llvm::APFloat(1.0));
+	}
+
+	auto NextVar = Builder.CreateFAdd(Variable, StepVal, "nextvar");
+
+	//Compute the end condition.
+	auto EndCond = End->Codegen();
+	if (!EndCond)
+		return EndCond;
+
+	//Convert condition to a bool by comparing equal to 0.0.
+	EndCond = Builder.CreateFCmpONE(
+		EndCond, llvm::ConstantFP::get(llvm::getGlobalContext(), llvm::APFloat(0.0)), "loopcond");
+
+	//Create the "after loop" block and insert it.
+	auto LoopEndBB = Builder.GetInsertBlock();
+	auto AfterBB =
+		llvm::BasicBlock::Create(llvm::getGlobalContext(), "afterloop", TheFunction);
+
+	//Insert the conditional branch into the end of LoopEndBB.
+	Builder.CreateCondBr(EndCond, LoopBB, AfterBB);
+
+	//Any new code will be inserted in AfterBB.
+	Builder.SetInsertPoint(AfterBB);
+
+	//Add a new entry to the PHI node for the backedge.
+	Variable->addIncoming(NextVar, LoopEndBB);
+
+	//Restore the unshadowed variable.
+	if (OldVal)
+		NamedValues[VarName] = OldVal;
+	else
+		NamedValues.erase(VarName);
+
+	//for expr always returns 0.0.
+	return llvm::Constant::getNullValue(llvm::Type::getDoubleTy(llvm::getGlobalContext()));
 }
 
 llvm::Function* PrototypeAST::Codegen() {
@@ -532,7 +795,7 @@ int main() {
 #endif
 	//Create the JIT
 	std::string ErrStr;
-	llvm::EngineBuilder TheBuilder(TheModule);// std::unique_ptr<llvm::Module>(Owner));
+	llvm::EngineBuilder TheBuilder(TheModule);//std::unique_ptr<llvm::Module>(Owner));
 	llvm::RTDyldMemoryManager* RTDyldMM = new llvm::SectionMemoryManager();
 	TheBuilder.setErrorStr(&ErrStr);
 	//TheBuilder.setEngineKind(llvm::EngineKind::JIT);
